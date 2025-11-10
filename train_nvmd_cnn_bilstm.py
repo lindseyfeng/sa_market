76da8dd08598fcf6398cb339bbb9d389983108b7
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2025-11-09 22:15:38 -0500

    update

--- train_nvmd_cnn_bilstm.py
+++ train_nvmd_cnn_bilstm.py
@@ -204,7 +204,7 @@ def main():
     # Data
     ap.add_argument("--train-csv", default="VMD_modes_with_residual_2018_2021.csv")
     ap.add_argument("--val-csv",   default="VMD_modes_with_residual_2021_2022_with_EWT.csv")
-    ap.add_argument("--seq-len", type=int, default=32)
+    ap.add_argument("--seq-len", type=int, default=128)
 
     # Model
     ap.add_argument("--base", type=int, default=64)
@@ -215,11 +215,11 @@ def main():
 
     # Train
     ap.add_argument("--epochs", type=int, default=100)
-    ap.add_argument("--batch", type=int, default=256)
+    ap.add_argument("--batch", type=int, default=1024)
     ap.add_argument("--lr", type=float, default=5e-4)
     ap.add_argument("--alpha", type=float, default=1)      # weight for IMF MSE
     ap.add_argument("--beta",  type=float, default=0)      # weight for pred MSE
-    ap.add_argument("--clip-grad", type=float, default=30.0)
+    ap.add_argument("--clip-grad", type=float, default=100.0)
     ap.add_argument("--seed", type=int, default=42)
     ap.add_argument("--num-workers", type=int, default=0)
 
