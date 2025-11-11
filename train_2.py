8b6b077f74228d5a201aaf6065b7f64658bdf374
Xinyu Feng <xinyuf@usc.edu>
2025-11-11 15:03:00 -0500

    Update train_2.py

--- train_2.py
+++ train_2.py
@@ -295,7 +295,7 @@ def main():
             optimizer=None
         )
         if va_ld <= 7:
-            args.alpha, args.beta = 1, 0.5
+            args.alpha, args.beta = 0.7, 1.5
 
         print(f"[Epoch {ep:02d}] "
               f"train: total={tr_loss:.6f} decomp={tr_ld:.6f} pred={tr_lp:.6f} | "
