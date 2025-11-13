58b7e61a60122f13d56fb7aa52894f8f86f8eefe
Xinyu Feng <xinyuf@usc.edu>
2025-11-13 09:38:06 -0500

    Update train_nvmd_cnn_bilstm_together.py.py

--- train_nvmd_cnn_bilstm_together.py.py
+++ train_nvmd_cnn_bilstm_together.py.py
@@ -201,7 +201,7 @@ def main():
 
     # Training
     ap.add_argument("--epochs", type=int, default=20)
-    ap.add_argument("--batch", type=int, default=64)
+    ap.add_argument("--batch", type=int, default=128)
     ap.add_argument("--lr", type=float, default=1e-3)
     ap.add_argument("--alpha", type=float, default=1.0, help="weight for IMF reconstruction loss")
     ap.add_argument("--beta",  type=float, default=1.0, help="weight for next-step prediction loss")
