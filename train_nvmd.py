f0a100e0ce6f5691db255e231d537c30189449ef
Xinyu Feng <xinyuf@usc.edu>
2025-11-13 23:04:10 -0500

    Update train_nvmd.py

--- train_nvmd.py
+++ train_nvmd.py
@@ -283,7 +283,7 @@ def main():
     ap.add_argument("--val-csv", type=str,
                     default="VMD_modes_with_residual_2021_2022.csv")
     ap.add_argument("--rrp-col", type=str, default="RRP")
-    ap.add_argument("--seq-len", type=int, default=64)
+    ap.add_argument("--seq-len", type=int, default=256)
     ap.add_argument("--K", type=int, default=13)
 
     # training
@@ -297,9 +297,9 @@ def main():
                     help="weight for IMF window reconstruction loss")
     ap.add_argument("--w-rrp", type=float, default=0.1,
                     help="weight for RRP window reconstruction loss")
-    ap.add_argument("--w-smooth", type=float, default=1e-3,
+    ap.add_argument("--w-smooth", type=float, default=0.1,
                     help="weight for spectral smoothness of masks")
-    ap.add_argument("--w-ortho", type=float, default=1e-3,
+    ap.add_argument("--w-ortho", type=float, default=0.1,
                     help="weight for spectral orthogonality of masks")
 
     # I/O
