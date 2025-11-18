9c927dbf793e11fb5003526c81962019ab1a03c0
Xinyu Feng <xinyuf@usc.edu>
2025-11-18 11:16:10 -0500

    Update train_transformer.py

--- train_transformer.py
+++ train_transformer.py
@@ -380,9 +380,9 @@ def main():
     ap = argparse.ArgumentParser()
     # Data
     ap.add_argument("--train-csv", type=str,
-                    default="VMD_modes_with_residual_2018_2018.csv")
+                    default="VMD_modes_with_residual_2018_only.csv")
     ap.add_argument("--val-csv", type=str,
-                    default="VMD_modes_with_residual_2019_2019.csv")
+                    default="VMD_modes_with_residual_2019_only.csv")
     ap.add_argument("--rrp-col", type=str, default="RRP")
     ap.add_argument("--seq-len", type=int, default=128)
 
